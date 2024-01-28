e91e66988c51ab9465fa0cd961a293617757a2a9
a.daroczi <a.daroczi@resource-it.ro>
2024-01-28 21:16:14 +0200

    - Add TeslaModelSelectorComponent to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TeslaModelSelectorComponent } from './tesla-model-selector/tesla-model-selector.component';
+import { RoutingConstants } from './routing/routing-constants';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: RoutingConstants.getTeslaModelSelectorPagePath(),
+        component: TeslaModelSelectorComponent
+    }
+];
