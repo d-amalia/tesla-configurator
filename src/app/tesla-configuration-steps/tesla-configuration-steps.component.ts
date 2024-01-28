06b9581ccbb34c8da6ad4bba27384da0d272304b
a.daroczi <a.daroczi@resource-it.ro>
2024-01-28 19:59:51 +0200

    - Initialize configuration steps

--- src/app/tesla-configuration-steps/tesla-configuration-steps.component.ts
+++ src/app/tesla-configuration-steps/tesla-configuration-steps.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TeslaConfigurationStepsService } from './tesla-configuration-steps.service';
+import { TeslaConfigurationStep } from './tesla-configuration-step.model';
 
 @Component({
   selector: 'app-tesla-configuration-steps',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './tesla-configuration-steps.component.html',
   styleUrl: './tesla-configuration-steps.component.scss'
 })
-export class TeslaConfigurationStepsComponent {
+export class TeslaConfigurationStepsComponent implements OnInit {
 
+  steps: TeslaConfigurationStep[] = [];
+
+  constructor(private stepsService: TeslaConfigurationStepsService) {
+  }
+
+  ngOnInit(): void {
+    this.initializeSteps();
+  }
+
+  private initializeSteps(): void {
+    this.steps = this.stepsService.createConfigurationSteps();
+  }
 }
