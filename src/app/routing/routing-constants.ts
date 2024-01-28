ecba532a51c7f3cbb45da57e205710405e18b884
a.daroczi <a.daroczi@resource-it.ro>
2024-01-28 19:35:53 +0200

    - Add routing constants

---
+++ src/app/routing/routing-constants.ts
@@ -0,0 +1,25 @@
+
+const errorPagePath = 'error';
+
+const teslaModelSelectorPagePath = 'models';
+const teslaOptionsSelectorPagePath = 'options';
+const telsaConfigurationSummaryPagePath = 'summary';
+
+export class RoutingConstants {
+
+    public static getErrorPagePath(): string {
+        return errorPagePath;
+    }
+
+    public static getTeslaModelSelectorPagePath(): string {
+        return teslaModelSelectorPagePath;
+    }
+
+    public static getTeslaOptionsSelectorPagePath(): string {
+        return teslaOptionsSelectorPagePath;
+    }
+
+    public static getTelsaConfigurationSummaryPagePath(): string {
+        return telsaConfigurationSummaryPagePath;
+    }
+}
