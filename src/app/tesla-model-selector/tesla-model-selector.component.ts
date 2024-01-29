cb54e6f1c60c8c3de2fc4a7e2b29de43fc4955b4
a.daroczi <a.daroczi@resource-it.ro>
2024-01-29 20:42:26 +0200

    - Display tesla models in a select

--- src/app/tesla-model-selector/tesla-model-selector.component.ts
+++ src/app/tesla-model-selector/tesla-model-selector.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TeslaDataService } from '../services/tesla-data.service';
+import { TeslaModel } from './tesla-model.model';
+import { Observable } from 'rxjs';
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-tesla-model-selector',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    NgIf,
+    NgFor
+  ],
   templateUrl: './tesla-model-selector.component.html',
   styleUrl: './tesla-model-selector.component.scss'
 })
-export class TeslaModelSelectorComponent {
+export class TeslaModelSelectorComponent implements OnInit {
 
+  public teslaModels$!: Observable<TeslaModel[]>;
+
+  constructor(private teslaDataService: TeslaDataService) {
+  }
+
+  ngOnInit(): void {
+    this.initializeTeslaModels();
+  }
+
+  private initializeTeslaModels(): void {
+    this.teslaModels$ = this.teslaDataService.getTeslaModels();
+  }
 }
