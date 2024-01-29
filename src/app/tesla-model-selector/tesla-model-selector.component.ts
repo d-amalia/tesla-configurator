043fa03fbfb6b5b943b3c394073f54a672911623
a.daroczi <a.daroczi@resource-it.ro>
2024-01-29 21:41:25 +0200

    - Initialize model form control - Subscribe to model form control changes

--- src/app/tesla-model-selector/tesla-model-selector.component.ts
+++ src/app/tesla-model-selector/tesla-model-selector.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TeslaDataService } from '../services/tesla-data.service';
 import { TeslaModel } from './tesla-model.model';
-import { Observable } from 'rxjs';
-import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
+import { Observable, Subscription } from 'rxjs';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tesla-model-selector',
@@ -10,23 +11,48 @@ import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
   imports: [
     AsyncPipe,
     NgIf,
-    NgFor
+    NgFor,
+    ReactiveFormsModule
   ],
   templateUrl: './tesla-model-selector.component.html',
   styleUrl: './tesla-model-selector.component.scss'
 })
-export class TeslaModelSelectorComponent implements OnInit {
+export class TeslaModelSelectorComponent implements OnInit, OnDestroy {
 
   public teslaModels$!: Observable<TeslaModel[]>;
+  public modelControl!: FormControl<string | null>;
+
+  private subSink = new Subscription();
 
   constructor(private teslaDataService: TeslaDataService) {
   }
 
   ngOnInit(): void {
     this.initializeTeslaModels();
+    this.initializeModelControl();
+
+    this.onModelControlValueChange();
   }
 
   private initializeTeslaModels(): void {
     this.teslaModels$ = this.teslaDataService.getTeslaModels();
+  }
+
+  private initializeModelControl(): void {
+    this.modelControl = new FormControl<string | null>(null, {
+      validators: Validators.required
+    });
+  }
+
+  private onModelControlValueChange(): void {
+    const subscription = this.modelControl.valueChanges.subscribe((modelCode: string | null) => {
+      console.log(modelCode);
+    });
+
+    this.subSink.add(subscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subSink.unsubscribe();
   }
 }
