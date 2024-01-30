bd6e0cd34f6d086cbaef0e5b21866a3ea2957ff2
a.daroczi <a.daroczi@resource-it.ro>
2024-01-30 18:27:42 +0200

    - Refactor

--- src/app/tesla-configuration-summary/tesla-configuration.model.ts
+++ src/app/tesla-configuration-summary/tesla-configuration.model.ts
@@ -2,11 +2,11 @@ import { Observable } from "rxjs";
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 export class TeslaConfiguration {
-    public modelCode: FormControl<string | null>;
-    public colorCode: FormControl<string | null>;
-    public configId: FormControl<number | null>;
-    public includeYoke: FormControl<boolean>;
-    public includeTow: FormControl<boolean>;
+    modelCode: FormControl<string | null>;
+    colorCode: FormControl<string | null>;
+    configId: FormControl<number | null>;
+    includeYoke: FormControl<boolean>;
+    includeTow: FormControl<boolean>;
 
     constructor() {
         this.modelCode = new FormControl<string | null>(null, { validators: Validators.required });
@@ -20,64 +20,64 @@ export class TeslaConfiguration {
 export class TeslaConfigurationFormManager {
     private _form: FormGroup<TeslaConfiguration>;
 
-    public constructor() {
+    constructor() {
         const initialTeslaConfiguration = new TeslaConfiguration();
         this._form = new FormGroup<TeslaConfiguration>(initialTeslaConfiguration);
     }
 
-    public get form(): FormGroup<TeslaConfiguration> {
+    get form(): FormGroup<TeslaConfiguration> {
         return this._form;
     }
 
-    public get modelCodeControl(): FormControl<string | null> {
+    get modelCodeControl(): FormControl<string | null> {
         return this.form.controls.modelCode;
     }
 
-    public get colorCodeControl(): FormControl<string | null> {
+    get colorCodeControl(): FormControl<string | null> {
         return this.form.controls.colorCode;
     }
 
-    public get configIdControl(): FormControl<number | null> {
+    get configIdControl(): FormControl<number | null> {
         return this.form.controls.configId;
     }
 
-    public get includeYokeControl(): FormControl<boolean> {
+    get includeYokeControl(): FormControl<boolean> {
         return this.form.controls.includeYoke;
     }
 
-    public get includeTowControl(): FormControl<boolean> {
+    get includeTowControl(): FormControl<boolean> {
         return this.form.controls.includeTow;
     }
 
-    public get modelCodeControlValue(): string | null {
+    get modelCodeControlValue(): string | null {
         return this.modelCodeControl.value;
     }
 
-    public get modelCodeSelected(): boolean {
+    get modelCodeSelected(): boolean {
         return this.modelCodeControlValue !== null;
     }
 
-    public get modelCodeControlValueChanges(): Observable<string | null> {
+    get modelCodeControlValueChanges(): Observable<string | null> {
         return this.modelCodeControl.valueChanges;
     }
 
-    public setColorCodeControlValue(colorCode: string): void {
-        this.colorCodeControl.setValue(colorCode);
-    }
-
-    public get configIdControlValueChanges(): Observable<number | null> {
+    get configIdControlValueChanges(): Observable<number | null> {
         return this.configIdControl.valueChanges;
     }
 
-    public get configIdControlValue(): number | null {
+    get configIdControlValue(): number | null {
         return this.configIdControl.value;
     }
 
-    public resetConfigIdControl(): void {
-        return this.configIdControl.reset();
+    get colorCodeControlValue(): string | null {
+        return this.colorCodeControl.value;
     }
 
-    public get colorCodeControlValue(): string | null {
-        return this.colorCodeControl.value;
+    setColorCodeControlValue(colorCode: string): void {
+        this.colorCodeControl.setValue(colorCode);
+    }
+
+    resetConfigIdControl(): void {
+        return this.configIdControl.reset();
     }
 }
