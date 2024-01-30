13a9335954fbe53acf4e7f0c575f125a6cfe4c4d
a.daroczi <a.daroczi@resource-it.ro>
2024-01-30 07:20:35 +0200

    - Remove unused import

--- src/app/services/tesla-data.service.ts
+++ src/app/services/tesla-data.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError } from 'rxjs';
 import { TeslaModel } from '../tesla-model-selector/tesla-model.model';
