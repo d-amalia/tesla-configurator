3bbe06a5f1416425a9c10762ac2b6c3c2ed44ce2
a.daroczi <a.daroczi@resource-it.ro>
2024-01-30 07:57:47 +0200

    - Initialize hardcoded options data

--- src/app/tesla-options-selector/tesla-options-selector.component.ts
+++ src/app/tesla-options-selector/tesla-options-selector.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TeslaModelOptions } from './tesla-option.model';
+import { TeslaDataService } from '../services/tesla-data.service';
+import { Subscription } from 'rxjs';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-tesla-options-selector',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgFor
+  ],
   templateUrl: './tesla-options-selector.component.html',
   styleUrl: './tesla-options-selector.component.scss'
 })
-export default class TeslaOptionsSelectorComponent {
+export default class TeslaOptionsSelectorComponent implements OnInit, OnDestroy {
 
+  public dataLoaded: boolean = false;
+  public options!: TeslaModelOptions;
+
+  private subSink = new Subscription();
+
+  constructor(private dataService: TeslaDataService) {
+  }
+
+  ngOnInit(): void {
+    this.initializeModelOptionsData();
+  }
+
+  private initializeModelOptionsData(): void {
+    const modelCode = "X";
+    const subscription = this.dataService.getTeslaModelOptions(modelCode).subscribe((options) => {
+      this.onInitializedModelOptionsData(options);
+    });
+
+    this.subSink.add(subscription);
+  }
+
+  private onInitializedModelOptionsData(options: TeslaModelOptions): void {
+    this.options = options;
+
+    this.dataLoaded = true;
+  }
+
+  ngOnDestroy(): void {
+    this.subSink.unsubscribe();
+  }
 }
