13307bab67ad7fa29827d45723f0870e9bf11e9e
a.daroczi <a.daroczi@resource-it.ro>
2024-01-30 21:35:26 +0200

    - Use TeslaModelViewerComponent in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TeslaConfigurationStepsComponent } from './tesla-configuration-steps/tesla-configuration-steps.component';
+import { TeslaModelViewerComponent } from './tesla-model-viewer/tesla-model-viewer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterModule,
-    TeslaConfigurationStepsComponent
+    TeslaConfigurationStepsComponent,
+    TeslaModelViewerComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
